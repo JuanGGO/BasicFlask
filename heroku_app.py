1560f27787a77556d20859c3f5c614bd3536576e
juan <jggarciao2015@gmail.com>
2020-08-19 20:54:49 -0700

    second commit

--- heroku_app.py
+++ heroku_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,11 +8,20 @@ def hello_world():
     return '<h1>Hello World! Heroku<h1>'
 
 
-@app.route('/from_flutter', methods=['GET', 'POST'])
+@app.route('/from_flutter/', methods=['POST'])
 def get_signal_from_flutter():
     param = request.form.get('title')
     print(param)
+    if param:
+        return jsonify({
+            "Message": "yay flutter talked to me :)",
+            "flutter": param}
+        )
 
+    else:
+        return jsonify({
+            'ERROR': 'FU asshole'
+        })
 
 if __name__ == '__main__':
     app.run()
